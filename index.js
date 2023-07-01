c1f3660625fab6a3b080799279cae6826e6bac0e
Ashish Sethi <ashish.sethi@sociometrik.com>
2023-07-01 13:45:57 +0530

    url shortner

--- index.js
+++ index.js
@@ -1,66 +1,32 @@
-// Importing required modules
-const express = require("express");
-const bodyParser = require("body-parser");
-const shortid = require("shortid");
-const fs = require("fs");
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const shortid = require('shortid')
+const urlData = require('./urls.json')
+const fs = require('fs')
+app
+	.use(bodyParser.json())
+	.get('/:shortURL', (req, res) => {
+		const { shortURL } = req.params
+		const foundUrlObj = urlData.find((obj) => obj?.short_url === shortURL)
+		console.log('foundUrlObj', foundUrlObj)
+		if (foundUrlObj) {
+			res.redirect(foundUrlObj.url)
+		} else {
+			res.send('No url found')
+		}
+	})
+	.post('/shorten', (req, res) => {
+		const shortURL = shortid.generate()
+		const obj = {}
+		obj['url'] = req.body.url
+		obj['short_url'] = shortURL
 
-// Creating an instance of express
-const app = express();
-
-// Middleware to parse the incoming request body
-app.use(bodyParser.json());
-
-// Read the existing URLs file
-let urlsData = require("./urls.json");
-
-// Route to shorten a URL
-app.post("/shorten", (req, res) => {
-  // Extract the URL from request body
-  const originalUrl = req.body.url;
-
-  // Check if the URL is present in the JSON data
-  const foundUrl = urlsData.find((item) => item.original_url === originalUrl);
-
-  if (foundUrl) {
-    // If the URL is already present, return the existing record
-    res.send(foundUrl);
-  } else {
-    // If not, create a new record
-    const shortUrl = shortid.generate();
-
-    // New URL object
-    const newUrl = {
-      original_url: originalUrl,
-      short_url: shortUrl,
-    };
-
-    // Add the new URL to the array
-    urlsData.push(newUrl);
-
-    // Write the new array to the file
-    fs.writeFileSync("./urls.json", JSON.stringify(urlsData));
-
-    // Return the new URL record
-    res.send(newUrl);
-  }
-});
-
-// Route to redirect to the original URL
-app.get("/:shortUrl", (req, res) => {
-  // Extract the short URL from the request parameters
-  const shortUrl = req.params.shortUrl;
-
-  // Find the record with this short URL
-  const foundUrl = urlsData.find((item) => item.short_url === shortUrl);
-
-  if (foundUrl) {
-    // If found, redirect to the original URL
-    res.redirect(foundUrl.original_url);
-  } else {
-    // If not found, send an error message
-    res.send({ error: "URL not found" });
-  }
-});
-
-// Start the server
-app.listen(3000, () => console.log("Server started on port 3000"));
+		urlData.push(obj)
+		console.log(urlData)
+		fs.writeFileSync('urls.json', JSON.stringify(urlData))
+		res.send(shortURL).status(200)
+	})
+	.listen(3000, () => {
+		console.log('server started')
+	})
